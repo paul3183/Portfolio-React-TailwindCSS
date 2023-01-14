c93b935db3046b29d4d03b399c520870f99a79c8
Paul Ruiz <ruizp3183@gmail.com>
2023-01-14 00:57:27 -0500

    certificated

--- src/components/CertificatedStudent.jsx
+++ src/components/CertificatedStudent.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import bachi from '../assets/certificatesFree/bachidelante.jpg'
+import fundamentos from '../assets/certificatesFree/fundamentos.webp'
+import react from '../assets/certificatesFree/react.webp'
 
 const CertificatedStudent = () => {
   return (
-    <div>
-      <h2>Academic certificates</h2>
-      <img src={bachi} alt="bachiller" />
+    <div className="w-64 carousel rounded-box">
+      <div className="carousel-item w-full">
+        <img src={bachi} className="w-full" alt="Tailwind CSS Carousel component" />
+      </div>
+      <div className="carousel-item w-full">
+        <img src={fundamentos} className="w-full" alt="Tailwind CSS Carousel component" />
+      </div>
+      <div className="carousel-item w-full">
+        <img src={react} className="w-full" alt="Tailwind CSS Carousel component" />
+      </div>
     </div>
   )
 }
--- src/components/CertificatedWorks.jsx
+++ src/components/CertificatedWorks.jsx
@@ -3,16 +3,33 @@ import azteca from '../assets/certificatesFree/AZTECA.webp'
 import mibanco from '../assets/certificatesFree/MIBANCO.webp'
 import wong from '../assets/certificatesFree/WONG.webp'
 import srosa from '../assets/certificatesFree/SANTA-ROSA.webp'
+import sunat from '../assets/certificatesFree/sunat.webp'
 
 const CertificatedWorks = () => {
   return (
     <div>
       <h2>Labor Certificates</h2>
-      <img src={azteca} alt="azteca certificate" />
-      <img src={mibanco} alt="mibanco" />
-      <img src={wong} alt="wong" />
-      <img src={srosa} alt="srosa" />
+      <div className="w-64 carousel rounded-box">
+        <div className="carousel-item w-full">
+          <img src={mibanco} className="w-full" alt="Tailwind CSS Carousel component" />
+        </div>
+        <div className="carousel-item w-full">
+          <img src={azteca} className="w-full" alt="Tailwind CSS Carousel component" />
+        </div>
+
+        <div className="carousel-item w-full">
+          <img src={wong} className="w-full" alt="Tailwind CSS Carousel component" />
+        </div>
+        <div className="carousel-item w-full">
+          <img src={srosa} className="w-full" alt="Tailwind CSS Carousel component" />
+        </div>
+        <div className="carousel-item w-full">
+          <img src={sunat} className="w-full" alt="Tailwind CSS Carousel component" />
+        </div>
+      </div>
     </div>
+
+
   )
 }
 
